24faf87223f8cf6d96372bbff60a9d5b1a29de84
Mihir Solankar <mihir.solankar1@gmail.com>
2022-07-06 13:28:36 +0530

    Annotations update

--- Features/annotations/functions.ts
+++ Features/annotations/functions.ts
@@ -3,3 +3,16 @@
 const add = (a: number, b: number): number => {
   return a + b;
 };
+
+function divide(a: number, b: number): number {
+  return a / b;
+}
+
+function logger(name: string): void {
+  console.log(name);
+}
+
+//never should be used for cases like infinite loops or as below
+const throwError = (message: string): never => {
+  throw new Error(message);
+};

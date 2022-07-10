814a5bbeb0a7f7d5b5df0a19bc78f67c2033a9de
Mihir S <mihir.solankar1@gmail.com>
2022-07-10 16:15:47 +0530

    Added Map Markers

---
+++ AppExamples/Example_One/src/CustomMap.ts
@@ -0,0 +1,42 @@
+import { User } from "./User";
+import { Company } from "./Company";
+
+export class CustomMap {
+  private googleMap: google.maps.Map;
+
+  constructor(divId: string) {
+    /**
+     * Typescript also has a non-null assertion that you can use when you are sure that the
+     * value is never null by adding the ! operator to the end of your statement
+     */
+    this.googleMap = new google.maps.Map(document.getElementById(divId)!, {
+      zoom: 1,
+      center: {
+        lat: 0,
+        lng: 0,
+      },
+    });
+  }
+
+  addMarker(mappable: User | Company): void {
+    new google.maps.Marker({
+      map: this.googleMap,
+      position: {
+        lat: mappable.location.latitude,
+        lng: mappable.location.longitude,
+      },
+    });
+  }
+
+  //addCompanyMarker(company: Company): void {}
+
+  //   addMarker(user: User, company: Company): void {
+  //     new google.maps.Marker({
+  //       map: this.googleMap,
+  //       position: {
+  //         lat: user.location.latitude | company.location.latitude,
+  //         lng: user.location.longitude | company.location.longitude,
+  //       },
+  //     });
+  //   }
+}
